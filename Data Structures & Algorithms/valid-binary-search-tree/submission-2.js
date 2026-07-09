f053e20568898cac5a1c6c24e1106574588ffccf
Dhanush M <dhanush718718m@gmail.com>
2026-07-09 23:41:40 +0530

    Add: valid-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-2.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isValidBST(root) {
+
+       function validate(node,lower,upper){
+        if(node===null){
+            return true
+        }
+
+        if(node.val <=lower || node.val>=upper){
+            return false;
+        }
+        return validate(node.left,lower,node.val)&&validate(node.right,node.val,upper)
+
+       }
+       return validate(root,-Infinity,Infinity);
+
+    }
+}
