a71b8156ad26cf33276d0b8ee02573aeba512f24
Dhanush M <dhanush718718m@gmail.com>
2026-07-25 08:41:42 +0530

    Add: depth-of-binary-tree - submission-4

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-4.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root) {
+
+        if(root==null){
+            return 0
+        }
+
+        let leftRoot= this.maxDepth(root.left);
+        let rightRoot= this.maxDepth(root.right);
+
+        return 1+Math.max(leftRoot,rightRoot);
+
+
+
+    }
+
+    
+
+}
