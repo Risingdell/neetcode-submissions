03d782a603ecde24505b1ef2083949dc0f459aca
Dhanush M <dhanush718718m@gmail.com>
2026-07-27 16:39:24 +0530

    Add: is-anagram - submission-37

---
+++ Data Structures & Algorithms/is-anagram/submission-37.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(t.length!==s.length){return false}
+        let map=new Map();
+
+        for(let i=0;i<s.length;i++){
+            map.set(s[i],(map.get(s[i])||0)+1);
+        }
+        for(let i=0;i<t.length;i++){
+            if(map.has(t[i])){
+                map.set(t[i],(map.get(t[i])||0)-1)
+            }else{
+                return false;
+            }
+        }
+
+        for(let [key,val] of map){
+            if(val!=0){
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+}
