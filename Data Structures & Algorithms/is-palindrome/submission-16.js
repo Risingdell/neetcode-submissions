425ea9fb30c1b9ae5a0122cd6d50e9ba242c678b
Dhanush M <dhanush718718m@gmail.com>
2026-08-09 23:37:11 +0530

    Add: is-palindrome - submission-16

---
+++ Data Structures & Algorithms/is-palindrome/submission-16.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let str=s.toLowerCase().replace(/[^a-z0-9]/g,"")
+        
+        let i=0;
+        let j=str.length-1;
+
+        while(i<j){
+            if(str[i]!==str[j]){
+                return false
+            }
+            i++;
+            j--;
+        }
+        return true
+    }
+}
