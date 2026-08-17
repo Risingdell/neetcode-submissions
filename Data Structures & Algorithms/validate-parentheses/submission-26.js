85ccde043745a3b151c4207c7ac2e52902be185a
Dhanush M <dhanush718718m@gmail.com>
2026-08-17 15:55:09 +0530

    Add: validate-parentheses - submission-26

---
+++ Data Structures & Algorithms/validate-parentheses/submission-26.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack=[];
+        for(let char of s ){
+            if(char=='('|| char=='{'|| char=='['){
+                stack.push(char)
+            }else{
+                let top=stack.pop();
+
+
+                if((char==="}" && top!=="{" )||(char==="]"&& top!=="[")||(char===")"&& top!=="(")){
+                    return false;
+                }
+
+                
+            }
+        }
+        return stack.length===0;
+    }
+}
